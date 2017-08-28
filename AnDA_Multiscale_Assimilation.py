db4c55f830c529629472534a2866c5fc59cf47cc
vietphi3892 <vietphi3892@gmail.com>
2017-08-28 10:11:21 +0200

    Update AnDA_Multiscale_Assimilation.py

--- AnDA_Multiscale_Assimilation.py
+++ AnDA_Multiscale_Assimilation.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python
-
-""" AnDA_Multiscale_Assimilation.py: Apply multi-scale, patch-based stochastic and sequential data assimilation techniques using analog forecasting. """
-
-__author__ = "Phi Huynh Viet"
-__version__ = "1.0"
-__date__ = "2017-08-01"
-__email__ = "phi.huynhviet@telecom-bretagne.eu"
-
-
 import numpy as np
 from pyflann import *
 from sklearn.decomposition import PCA
@@ -40,6 +30,7 @@ class Multiscale_Assimilation:
         self.AF = _AF
     def single_patch_assimilation(self,coordinate):
         """  single patch assimilation """
+        
         global VAR
         mkl.set_num_threads(1)
         # position of patch
@@ -52,23 +43,27 @@ class Multiscale_Assimilation:
         # find sea_mask to remove land pixel
         sea_mask = VAR.dX_GT_test[0,r[0]:r[-1]+1,c[0]:c[-1]+1].flatten()
         sea_mask = np.where(~np.isnan(sea_mask))[0]    
+            
         # use classic assmilation (without any condition) at border    
-        if (len(sea_mask)!=self.PR.patch_r*self.PR.patch_c): # bordering patches, reset to classic AF
+        if ((len(sea_mask)!=self.PR.patch_r*self.PR.patch_c) or self.PR.flag_scale): # bordering patches, reset to classic AF
             AF.flag_catalog = False
-            AF.flag_model = False
-            AF.flag_scale = False 
+
         # observation for this patch    
         obs_p = VAR.Obs_test[:,r[0]:r[-1]+1,c[0]:c[-1]+1]
         obs_p = obs_p.reshape(obs_p.shape[0],-1)
         obs_p_no_land = obs_p[:,sea_mask]   
         AF.obs_mask = obs_p_no_land
-        if (AF.flag_scale):
-            # retrieving neighbor patchs
-            k_patch = VAR.index_patch.keys()[VAR.index_patch.values().index([r[0],c[0]])]
-            listpos = VAR.neighbor_patchs[k_patch] 
-            listpos = sum(list(map((lambda x:range(x*self.PR.training_days,(x+1)*self.PR.training_days)),listpos)),[])      
-            # remove last patch at each position out of retrieving analogs
-            jet = len(listpos)/self.PR.training_days
+        if (AF.flag_catalog):
+            try:
+                # retrieving neighbor patchs
+                k_patch = VAR.index_patch.keys()[VAR.index_patch.values().index([r[0],c[0]])]
+                listpos = VAR.neighbor_patchs[k_patch] 
+                listpos = sum(list(map((lambda x:range(x*self.PR.training_days,(x+1)*self.PR.training_days)),listpos)),[])      
+                # remove last patch at each position out of retrieving analogs
+                jet = len(listpos)/self.PR.training_days
+            except ValueError:
+                print "Cannot define position of patch!!!"
+                quit()
         else:
             k_patch = 0
             jet = 1                  
@@ -77,7 +72,7 @@ class Multiscale_Assimilation:
             index_rem.append(np.arange(self.PR.training_days-i_lag-1,self.PR.training_days*(jet+1)-1,self.PR.training_days))
         AF.check_indices = np.array(index_rem).flatten()    
         # specify kind of AF depending on the position of patch (at border or not)  
-        if (len(sea_mask)==self.PR.patch_r*self.PR.patch_c): # not bordering patches
+        if ((len(sea_mask)==self.PR.patch_r*self.PR.patch_c) and self.PR.flag_scale): # not bordering patches
             if (AF.flag_catalog):
                 AF.catalogs = VAR.dX_train[listpos,:]
             else:
@@ -85,7 +80,8 @@ class Multiscale_Assimilation:
             AF.coeff_dX = VAR.dX_eof_coeff
             AF.mu_dX = VAR.dX_eof_mu         
         else: # bordering patches
-            patch_dX = VAR.dX_orig[:self.PR.training_days,r[0]:r[-1]+1,c[0]:c[-1]+1]
+            #patch_dX = VAR.dX_orig[:self.PR.training_days,r[0]:r[-1]+1,c[0]:c[-1]+1]
+            patch_dX = VAR.dX_orig[:,r[0]:r[-1]+1,c[0]:c[-1]+1]
             patch_dX = patch_dX.reshape(patch_dX.shape[0],-1)
             patch_dX_no_land = patch_dX[:,sea_mask]
             if (len(sea_mask)>=self.PR.n):
@@ -97,6 +93,8 @@ class Multiscale_Assimilation:
             AF.catalogs = pca.fit_transform(patch_dX_no_land)
             AF.coeff_dX = pca.components_.T
             AF.mu_dX = pca.mean_
+            if (self.PR.flag_scale):
+                AF.flag_model = False
         # Specify Observation
         class yo:
             time = np.arange(0,len(obs_p_no_land))
@@ -116,15 +114,36 @@ class Multiscale_Assimilation:
         AF.list_kdtree = list_kdtree
         # Specify physical model as conditions for AF
         if (AF.flag_model):
-            gr_vl_test_p = VAR.gr_vl_test[k_patch*len(VAR.dX_GT_test):(k_patch+1)*len(VAR.dX_GT_test),:]
+            try:
+                vx_test_p = VAR.gr_vl_test['vx'][:,r[0]:r[-1]+1,c[0]:c[-1]+1]
+                vx_test_p = np.dot(vx_test_p.reshape(vx_test_p.shape[0],-1)-VAR.gr_vl_coeff['vx_eof_mu'],VAR.gr_vl_coeff['vx_eof_coeff'])
+                vy_test_p = VAR.gr_vl_test['vy'][:,r[0]:r[-1]+1,c[0]:c[-1]+1]
+                vy_test_p = np.dot(vy_test_p.reshape(vy_test_p.shape[0],-1)-VAR.gr_vl_coeff['vy_eof_mu'],VAR.gr_vl_coeff['vy_eof_coeff'])
+                gx_test_p = VAR.gr_vl_test['gx'][:,r[0]:r[-1]+1,c[0]:c[-1]+1]
+                gx_test_p = np.dot(gx_test_p.reshape(gx_test_p.shape[0],-1)-VAR.gr_vl_coeff['gx_eof_mu'],VAR.gr_vl_coeff['gx_eof_coeff'])
+                gy_test_p = VAR.gr_vl_test['gy'][:,r[0]:r[-1]+1,c[0]:c[-1]+1]
+                gy_test_p = np.dot(gy_test_p.reshape(gy_test_p.shape[0],-1)-VAR.gr_vl_coeff['gy_eof_mu'],VAR.gr_vl_coeff['gy_eof_coeff'])
+                gr_vl_test_p = np.concatenate((vx_test_p[:,:], vy_test_p[:,:]),axis=1)
+                if (AF.flag_catalog):
+                    AF.cata_model_full = VAR.gr_vl_train[listpos,:] 
+                else:
+                    AF.cata_model_full = VAR.gr_vl_train 
+            except ValueError:
+                print "Cannot find physical model for AF !!!"
+                quit()
         else:
+            AF.cata_model_full = []
             gr_vl_test_p = None
         AF.x_model = gr_vl_test_p  
-        AF.cata_model_full = VAR.gr_vl_train         
+                
         # Specify dX condition for retrieving analogs
         if (AF.flag_cond):
-            dX_cond_p = VAR.dX_cond[:,r[0]:r[-1]+1,c[0]:c[-1]+1]
-            dX_cond_p = dX_cond_p.reshape(dX_cond_p.shape[0],-1)[:,sea_mask]  
+            try:
+                dX_cond_p = VAR.dX_cond[:,r[0]:r[-1]+1,c[0]:c[-1]+1]
+                dX_cond_p = dX_cond_p.reshape(dX_cond_p.shape[0],-1)[:,sea_mask]  
+            except ValueError:
+                print "Cannot find dX condition for AF !!!"
+                quit()
         else:
             dX_cond_p = None
         AF.x_cond = dX_cond_p      
@@ -135,7 +154,11 @@ class Multiscale_Assimilation:
             xb = np.dot(VAR.dX_GT_test[0,r[0]:r[-1]+1,c[0]:c[-1]+1].flatten()[sea_mask]-AF.mu_dX,AF.coeff_dX)
             B = AF.B * np.eye(AF.coeff_dX.shape[1])
             H = AF.coeff_dX
-            R = AF.R * np.eye(len(sea_mask))   
+            if (self.PR.flag_scale):
+                R = AF.R
+            else:
+                R = AF.R * np.eye(len(sea_mask))   
+                #R = AF.R
             @staticmethod
             def m(x,in_x): # x: query point at time t, in_x: index of condition at time t+lag
                 return AnDA_AF(x, in_x, AF)        
@@ -149,7 +172,7 @@ class Multiscale_Assimilation:
         dX_interpolated[:,:res_sst.shape[1],:res_sst.shape[2]] = res_sst
         return dX_interpolated
  
-    def multi_patches_assimilation(self, saved_path, level, r_start, r_length, c_start, c_length):
+    def multi_patches_assimilation(self, level, r_start, r_length, c_start, c_length):
         """ multi patches assimilation       
         level: 1 for series assimilation; >1 for parallel assimilation 
         """
@@ -159,7 +182,7 @@ class Multiscale_Assimilation:
         AnDA_result_test.GT = VAR.dX_GT_test[:,r_start:r_start+r_length,c_start:c_start+c_length] + AnDA_result_test.LR
         AnDA_result_test.Obs = VAR.Obs_test[:,r_start:r_start+r_length,c_start:c_start+c_length] + AnDA_result_test.LR
         AnDA_result_test.itrp_OI = VAR.Optimal_itrp[:,r_start:r_start+r_length,c_start:c_start+c_length] + AnDA_result_test.LR
-        AnDA_result_test.corr_OI = AnDA_correlate(AnDA_result_test.itrp_OI,AnDA_result_test.GT)
+        AnDA_result_test.corr_OI = AnDA_correlate(AnDA_result_test.itrp_OI-AnDA_result_test.LR,AnDA_result_test.GT-AnDA_result_test.LR)
         AnDA_result_test.itrp_AnDA = np.nan*np.zeros((len(VAR.Obs_test),r_length,c_length)) 
         ###########
         mask_sample = VAR.dX_GT_test[0,:,:]  
@@ -206,12 +229,9 @@ class Multiscale_Assimilation:
             r_sub = r_sub[r_sub<r_length+r_start]
             c_sub  = np.arange(c_start,c_start+self.PR.patch_c) 
             
-        AnDA_result_test.corr_AnDA = AnDA_correlate(AnDA_result_test.itrp_AnDA,AnDA_result_test.GT)    
+        AnDA_result_test.corr_AnDA = AnDA_correlate(AnDA_result_test.itrp_AnDA,AnDA_result_test.GT-AnDA_result_test.LR)    
         AnDA_result_test.itrp_AnDA = AnDA_result_test.itrp_AnDA + AnDA_result_test.LR
         AnDA_result_test.rmse_AnDA = AnDA_RMSE(AnDA_result_test.itrp_AnDA,AnDA_result_test.GT)        
         AnDA_result_test.rmse_OI = AnDA_RMSE(AnDA_result_test.itrp_OI,AnDA_result_test.GT)
-          
-        # Store AnDA result
-        with open(saved_path, 'wb') as handle:
-            pickle.dump(AnDA_result_test, handle)
+
         return AnDA_result_test
